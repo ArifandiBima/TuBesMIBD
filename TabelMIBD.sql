05e7c3091cf67ed5aeaab4d7887ec96cd04871df
LelipalyEric <lelipalyeric@gmail.com>
2025-06-08 23:29:58 +0700

    Adjusting Header
    pp

--- TabelMIBD.sql
+++ TabelMIBD.sql
@@ -1,26 +1,28 @@
-DROP TABLE Kanal
-
 CREATE TABLE Kanal(
 	idKanal varchar(25) PRIMARY KEY,
 	nama varchar(25),
 	isGroup INT,
 	pass INT,
-	--gambar BLOB,
+	gambar varchar(50),
 	link varchar(50),
 	dscp varchar(100)
 )
+
 CREATE TABLE Pengguna(
 	email varchar(30) PRIMARY KEY,
 	idKanalPribadi varchar(25) FOREIGN KEY REFERENCES Kanal
 )
+
 CREATE TABLE Subscription(
 	idKanal1 varchar(25) FOREIGN KEY REFERENCES Kanal,
 	idKanal2 varchar(25) FOREIGN KEY REFERENCES Kanal
 )
+
 CREATE TABLE Posisi(
 	accessGroup INT PRIMARY KEY,
 	gelar varchar(15)
 )
+
 CREATE TABLE PosisiPenggunaGroup(
 	email varchar(30) FOREIGN KEY REFERENCES Pengguna,
 	idKanal varchar(25) FOREIGN KEY REFERENCES Kanal,
@@ -30,38 +32,40 @@ CREATE TABLE PosisiPenggunaGroup(
 )
 
 CREATE TABLE Video (
-    idVideo varchar(15) PRIMARY KEY,
-    description varchar(500),
+    idVideo varchar(50) PRIMARY KEY,
+    dscp varchar(500),
     judul varchar(30),
     waktu datetime,
 	isPublished INT,
 	uploader varchar(30) FOREIGN KEY REFERENCES Pengguna,
-	location varchar(25) FOREIGN KEY REFERENCES Kanal
+	loc varchar(25) FOREIGN KEY REFERENCES Kanal
 )
+
 CREATE TABLE Caption (
     idPengunggah varchar(30) FOREIGN KEY REFERENCES Pengguna,
-	idVideo varchar(15) FOREIGN KEY REFERENCES Video,
+	idVideo varchar(50) FOREIGN KEY REFERENCES Video,
 	subtitle varchar(50),
-	timestamp INT
+	waktu INT
 )
 CREATE TABLE Tonton (
 	idPenonton varchar(25) FOREIGN KEY REFERENCES Kanal,
-	idVideo varchar(15) FOREIGN KEY REFERENCES Video,
+	idVideo varchar(50) FOREIGN KEY REFERENCES Video,
 	likes INT,
 	duration INT,
 	PRIMARY KEY (idPenonton, idVideo)
 )
 CREATE TABLE Thread(
-	idVideo varchar(15) FOREIGN KEY REFERENCES Video,
+	idVideo varchar(50) FOREIGN KEY REFERENCES Video,
 	number INT,
 	PRIMARY KEY (idVideo, number)
 )
 CREATE TABLE Komentar(
-	idVideo varchar(15),
+	idVideo varchar(50),
 	threadNumber INT,
 	FOREIGN KEY (idVideo, threadNumber) REFERENCES Thread,
 	Waktu datetime,
 	uploader varchar(25) FOREIGN KEY REFERENCES Kanal,
-	message varchar(200),
+	msg varchar(200),
 	PRIMARY KEY (idVideo, threadNumber, Waktu, uploader)
 )
+
