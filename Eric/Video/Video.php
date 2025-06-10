885d5d4d98008ada7c7af6e48f8094d21586e0d5
LelipalyEric <lelipalyeric@gmail.com>
2025-06-10 11:06:56 +0700

    video belom comment

--- Eric/Video/Video.php
+++ Eric/Video/Video.php
@@ -5,11 +5,10 @@
     $connectionInfo = array( "Database"=>"master");
     $conn = sqlsrv_connect( $serverName, $connectionInfo);
     $imgSrc = "default.jpg";
-    if (isset($_SESSION)) {
+    if (isset($_SESSION["gambarnya"])) {
       $imgSrc = $_SESSION["gambarnya"];
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,25 +20,26 @@
 <body>
 
 <header>
-    <div class="logo" href="/devi/LandingPage.php">
-      <a href="">
-        <img src="/devi/logo_youtube.png" alt="YouTube Logo" />
+    <div class="logo">
+      <a href="/devi/LandingPage.php">
+        <img src="/devi/logo_youtube.png" alt="YouTube Logo"/>
       </a>
     </div>
     <div class="search-bar">
-      <input type="text" placeholder="Search" />
+      <input type="text" placeholder="Search"/>
       <a href="">
-        <img src="/devi/search_icon.png" alt="Search Icon" />
+        <img src="/devi/search_icon.png" alt="Search Icon"/>
       </a>
     </div>
     <div class="actions">
-      <a href="">
+      <a href="/HasilBima/Notif/NotifReject.php">
         <img src="/devi/notif-icon.png" alt="Notifications" />
       </a>
       <div class="profile-circle">
-        <a href="">A</a>
+        
+        <a href="<?php if (!isset($_SESSION["email"])) echo "/HasilBima/Login/EmailRequest.php"; else echo "/Eric/Profile/Profile.php"?>">A</a>
       </div>
-    </div>
+    </div> 
   </header>
 
   <main class="video-page">
@@ -47,21 +47,52 @@
     <div class="video-main">
       <div class="video-player">
         <video controls>
-          <source src="<?php $_GET("idVideo")?>" type="video/mp4">
+          <source src="<?php echo $_GET["idVideo"];?>" type="video/mp4">
+          Your browser does not support HTML5 video.
         </video>
       </div>
 
       <!-- Uploader Info -->
       <div class="uploader-info">
-        <img src="https://via.placeholder.com/50" alt="Uploader Logo" />
-        <div>
-          <h4>Channel Name</h4>
-          <button class="subscribe">Subscribe</button>
-        </div>
+        <?php
+        $myquery = "
+        SELECT nama, gambar
+        FROM Kanal
+        WHERE idKanal = (
+          SELECT loc
+          FROM Video
+          WHERE idVideo=?
+        )";
+      
+        $stmt = sqlsrv_query($conn, $myquery, array( $_GET["idVideo"] ));
+        if ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+          $nama = $row["nama"]; 
+          $gmabar = $row["gambar"]; 
+          echo "<img src='$gmabar' alt='Uploader Logo' />
+            <div>
+            <h4>$nama</h4>
+            <button class='subscribe'>Subscribe</button>
+            </div>";
+        } 
+        ?>
+        
       </div>
 
       <!-- Video Title -->
-      <h2 class="video-title">How to Clone YouTube in HTML & CSS</h2>
+      <h2 class="video-title">
+      <?php
+      $myquery = "
+      SELECT judul
+      FROM Video
+      WHERE idVideo = ?";
+    
+      $stmt = sqlsrv_query($conn, $myquery, array( $_GET["idVideo"] ));
+      if ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+        $title = $row["judul"];  
+        echo "$title";
+      } 
+      ?>
+      </h2>
 
       <!-- Actions -->
       <div class="video-actions">
@@ -75,15 +106,25 @@
     <aside class="video-sidebar">
       <h3>Up Next</h3>
 
-      <div class="suggested-video">
-        <img src="https://via.placeholder.com/160x90" alt="Thumbnail" />
-        <div>
-          <p class="title">Learn CSS Grid Fast</p>
-          <p class="views">80K views</p>
-        </div>
-      </div>
-
-      <?php
+      <?php 
+      $myquery = "
+        SELECT TOP 3 idVideo, judul, thumbnail
+        FROM Video
+        WHERE isPublished = 1 and idVideo!= ?
+        ORDER BY RAND()";
+      
+      $stmt = sqlsrv_query($conn, $myquery, Array($_GET["idVideo"]));
+      while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC) ) {
+          $idVideo = $row["idVideo"];
+          $thumb = $row["thumbnail"];
+          $title = $row["judul"];
+          echo "<div class='suggested-video'>
+            <img src='$idVideo' href='../Eric/Video/Video.php?idVideo=$idVideo' alt='Thumbnail' />
+            <div>
+              <p class='title'>$title</p>
+            </div>
+            </div>";
+      }
       ?>
 
     </aside>
--- devi/LandingPage.php
+++ devi/LandingPage.php
@@ -17,28 +17,28 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>YouTube UI</title>
-  <link rel="stylesheet" href="landingPageStyles.css"/>
+  <link rel="stylesheet" href="/devi/landingPageStyles.css"/>
 </head>
 <body>
   <header>
     <div class="logo">
-      <a href="LandingPage.php">
-        <img src="logo_youtube.png" alt="YouTube Logo"/>
+      <a href="/devi/LandingPage.php">
+        <img src="/devi/logo_youtube.png" alt="YouTube Logo"/>
       </a>
     </div>
     <div class="search-bar">
       <input type="text" placeholder="Search"/>
       <a href="">
-        <img src="search_icon.png" alt="Search Icon"/>
+        <img src="/devi/search_icon.png" alt="Search Icon"/>
       </a>
     </div>
     <div class="actions">
       <a href="/HasilBima/Notif/NotifReject.php">
-        <img src="notif-icon.png" alt="Notifications" />
+        <img src="/devi/notif-icon.png" alt="Notifications" />
       </a>
       <div class="profile-circle">
         
-        <a href="<?php if (!isset($_SESSION["email"])) echo "../HasilBima/Login/EmailRequest.php"; else echo "/Eric/Profile/Profile.php"?>">A</a>
+        <a href="<?php if (!isset($_SESSION["email"])) echo "/HasilBima/Login/EmailRequest.php"; else echo "/Eric/Profile/Profile.php"?>">A</a>
       </div>
     </div>
   </header>
